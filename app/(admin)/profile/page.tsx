9b20e631f784a6fdc3ac8dcc9b8af433c7dda55d
faisalsaid <falsdesain@gmail.com>
2025-06-09 00:01:12 +0900

    populate user data on profile page

--- app/(admin)/profile/page.tsx
+++ app/(admin)/profile/page.tsx
@@ -1,18 +1,25 @@
-const ProfilePage = () => {
+import { auth } from '@/auth';
+
+const ProfilePage = async () => {
+  const session = await auth();
   return (
     <div className="flex flex-col xl:flex-row gap-8">
       <div className="w-full space-y-6 xl:w-2/3">
         <div className="bg-primary-foreground p-4 rounded-lg ">
-          <div className="mb-8 px-4 py-2 bg-secondary rounded-md">
-            <h1>All Category</h1>
+          <div className="mb-8 px-4 py-2 bg-secondary rounded-md space-y-4">
+            <h1>My Profile</h1>
+            <div className="space-y-2">
+              <p className="capitalize">{session?.user.name}</p>
+              <p>{session?.user.email}</p>
+              <p className="capitalize">
+                {session?.user.role.toLocaleLowerCase()}
+              </p>
+            </div>
           </div>
-          {/* <CategoryTable columns={categoryColumns} data={categories} /> */}
         </div>
       </div>
       <div className="w-full space-y-6 xl:w-1/3">
-        <div className="bg-primary-foreground p-4 rounded-lg ">
-          {/* <CategoryForm /> */}
-        </div>
+        <div className="bg-primary-foreground p-4 rounded-lg "></div>
         <div className="bg-primary-foreground p-4 rounded-lg "></div>
       </div>
     </div>
