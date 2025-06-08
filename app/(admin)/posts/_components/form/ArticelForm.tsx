2ab83acef1b7e962d6bfc4a1de6b93e2eb9609e7
faisalsaid <falsdesain@gmail.com>
2025-06-09 07:41:03 +0900

    add article schema

--- app/(admin)/posts/_components/form/ArticelForm.tsx
+++ app/(admin)/posts/_components/form/ArticelForm.tsx
@@ -1,4 +1,49 @@
-const ArticelForm = () => {
+'use client';
+
+import { postFormSchema } from '@/lib/zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSession } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const postStatus: string[] = ['DRAFT', 'REVIEW', 'PUBLISHED', 'ARCHIVED'];
+
+type Tag = { id: string; name: string; slug: string };
+
+type Props = {
+  initialData?: z.infer<typeof postFormSchema>;
+  //   categories: { id: string; name: string }[];
+  //   userId: string | undefined;
+  //   onSubmit: (data: FormSchema) => void;
+};
+
+const ArticelForm = ({ initialData }: Props) => {
+  const { data: session } = useSession();
+  const transformedInitialData = initialData
+    ? {
+        ...initialData,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        media: initialData.media?.map((item: any) => ({
+          id: item.mediaAsset?.id ?? item.id,
+          role: item.role,
+        })),
+      }
+    : undefined;
+
+  const form = useForm<z.infer<typeof postFormSchema>>({
+    resolver: zodResolver(postFormSchema),
+    defaultValues: transformedInitialData || {
+      title: '',
+      slug: '',
+      summary: '',
+      content: '',
+      categoryId: '',
+      status: 'DRAFT',
+      tags: [],
+      authorId: session?.user.id || '',
+      media: [],
+    },
+  });
   return (
     <>
       <div className="w-full space-y-6 xl:w-2/3">
