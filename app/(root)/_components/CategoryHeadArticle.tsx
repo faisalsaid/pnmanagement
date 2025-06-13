3981d552af9bca7562b9a8eb281fb7d98876d383
faisalsaid <falsdesain@gmail.com>
2025-06-14 07:51:02 +0900

    cagetory header handle issue if article have not asset feature

--- app/(root)/_components/CategoryHeadArticle.tsx
+++ app/(root)/_components/CategoryHeadArticle.tsx
@@ -4,6 +4,7 @@ import { getExcerptFromHtml } from '@/lib/helper/excerptAricle';
 import { Prisma } from '@prisma/client';
 import { CldImage } from 'next-cloudinary';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 interface Props {
   article: Prisma.ArticleGetPayload<{
@@ -18,28 +19,40 @@ interface Props {
   }>;
 }
 
+type MediaAsset = Props['article']['media'][number]['mediaAsset'];
+
 const CategoryHeadArticle = ({ article }: Props) => {
   //   console.log(article.media);
 
+  const [featureImage, setFeatureImage] = useState<MediaAsset>();
   const excerptArticle = getExcerptFromHtml(article.content, 30);
 
-  const { mediaAsset: fetureImage } = article.media.filter(
-    (item) => item.role === 'feature',
-  )[0];
+  useEffect(() => {
+    const feature = article.media.find((item) => item.role === 'feature');
+    if (feature) {
+      setFeatureImage(feature.mediaAsset);
+    }
+  }, [article.media]);
 
-  //   console.log(fetureImage);
+  console.log(article);
 
   return (
     <div className="flex gap-4 ">
-      <div className="flex-2/5 rounded-md overflow-hidden">
-        <CldImage
-          className="w-full h-full object-cover"
-          width={fetureImage.width as number}
-          height={fetureImage.height as number}
-          alt={fetureImage.public_id as string}
-          src={fetureImage.public_id as string}
-        />
-      </div>
+      {featureImage && (
+        <div className="flex-2/5 rounded-md overflow-hidden">
+          <CldImage
+            className="w-full h-full object-cover"
+            width={featureImage.width as number}
+            height={featureImage.height as number}
+            alt={featureImage.public_id as string}
+            src={
+              (featureImage.public_id as string) ||
+              (featureImage.secure_url as string)
+            }
+          />
+        </div>
+      )}
+
       <div className="flex-3/5 space-y-4">
         <h1 className="text-xl font-semibold">
           <Link href={`/read/${article.slug}`}>{article.title}</Link>
