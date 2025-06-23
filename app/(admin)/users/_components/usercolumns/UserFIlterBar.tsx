60f4c54a547a67c95973e70f37a159f1f6cc4df8
faisalsaid <falsdesain@gmail.com>
2025-06-24 08:30:51 +0900

    clear search input if all reset

--- app/(admin)/users/_components/usercolumns/UserFIlterBar.tsx
+++ app/(admin)/users/_components/usercolumns/UserFIlterBar.tsx
@@ -74,61 +74,65 @@ const UserFIlterBar = () => {
           </div>
         )}
       </div>
-      <Popover open={openRole} onOpenChange={setOpenRole}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            className="min-w-fit justify-start flex items-center gap-2"
-          >
-            <Filter />
-            <span>{selectedRole ? `Role: ${selectedRole}` : 'By Role'}</span>
-          </Button>
-        </PopoverTrigger>
-        {selectedRole && (
-          <div className="-ml-5">
-            <XCircle
-              size={16}
-              className="ml-2 cursor-pointer text-red-600"
-              onClick={(e) => {
-                e.stopPropagation();
-                updateParams((params) => params.delete('role'));
-              }}
-            />
-          </div>
-        )}
-        <PopoverContent className="w-[200px] p-0">
-          <Command>
-            <CommandInput placeholder="Search status..." />
-            <CommandList>
-              {role.map((item) => (
-                <CommandItem
-                  key={item}
-                  onSelect={() => {
-                    updateParams((params) => params.set('role', item));
-                    setOpenRole(false);
-                  }}
-                >
-                  {item}
-                </CommandItem>
-              ))}
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
 
-      {/* Reset All */}
-      {(search || selectedRole) && (
-        <Button
-          variant="ghost"
-          className="bg-red-600 text-white hover:text-white hover:bg-red-500 dark:hover:bg-red-500"
-          onClick={() => {
-            router.push('/users');
-            // setDateRange({ from: undefined, to: undefined });
-          }}
-        >
-          <RotateCcw /> <span>Reset All</span>
-        </Button>
-      )}
+      <div className="flex items-center gap-2 w-full pb-2 sm:pb-0">
+        <Popover open={openRole} onOpenChange={setOpenRole}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              className="min-w-[160px] justify-start flex items-center gap-2"
+            >
+              <Filter />
+              <span>{selectedRole ? `Role: ${selectedRole}` : 'By Role'}</span>
+            </Button>
+          </PopoverTrigger>
+          {selectedRole && (
+            <div className="-ml-5 ">
+              <XCircle
+                size={16}
+                className="ml-2 cursor-pointer text-red-600 bg-primary-foreground rounded-full"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  updateParams((params) => params.delete('role'));
+                }}
+              />
+            </div>
+          )}
+          <PopoverContent className="w-[200px] p-0">
+            <Command>
+              <CommandInput placeholder="Search status..." />
+              <CommandList>
+                {role.map((item) => (
+                  <CommandItem
+                    key={item}
+                    onSelect={() => {
+                      updateParams((params) => params.set('role', item));
+                      setOpenRole(false);
+                    }}
+                  >
+                    {item}
+                  </CommandItem>
+                ))}
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+
+        {/* Reset All */}
+        {(search || selectedRole) && (
+          <Button
+            variant="ghost"
+            className="bg-red-600 text-white hover:text-white hover:bg-red-500 dark:hover:bg-red-500"
+            onClick={() => {
+              setSearchText('');
+              router.push('/users');
+              // setDateRange({ from: undefined, to: undefined });
+            }}
+          >
+            <RotateCcw /> <span>Reset All</span>
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
