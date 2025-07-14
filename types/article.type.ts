96be8ac7b5e7eaae16290dd08e20d080344ad944
faisalsaid <falsdesain@gmail.com>
2025-07-14 16:44:49 +0900

    init TenPopularPostType and query

--- types/article.type.ts
+++ types/article.type.ts
@@ -24,3 +24,34 @@ export const getAllHeadlineArticleQuery =
 export type HeadlineArticleType = Prisma.ArticleGetPayload<
   typeof getAllHeadlineArticleQuery
 >;
+
+export const getTenPopularPostQuery =
+  Prisma.validator<Prisma.ArticleFindManyArgs>()({
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+      category: true,
+      createdAt: true,
+      viewCount: true,
+      media: {
+        where: {
+          role: 'feature',
+        },
+        select: {
+          mediaAsset: {
+            select: {
+              id: true,
+              public_id: true,
+              height: true,
+              width: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+export type TenPopularArticleType = Prisma.ArticleGetPayload<
+  typeof getTenPopularPostQuery
+>;
