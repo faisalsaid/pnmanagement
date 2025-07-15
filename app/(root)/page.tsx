b535bc8345f735fe274ccd7e6558a71d874140ea
faisalsaid <falsdesain@gmail.com>
2025-07-15 20:54:48 +0900

    tes render home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,48 +9,49 @@ import {
 } from '@/actions/postActions';
 
 const WebHomePage = async () => {
-  const { success: headStatus, result } = await getAllHeadlineArticle();
+  return <BlankHero />;
+  // const { success: headStatus, result } = await getAllHeadlineArticle();
 
-  const { success: categoryStatus, data } = await get3CategoriesForHome();
+  // const { success: categoryStatus, data } = await get3CategoriesForHome();
 
-  // console.log(categoryHome);
+  // // console.log(categoryHome);
 
-  return (
-    <main className="space-y-4">
-      {headStatus && result.length > 0 ? (
-        <HeroSection headlinePostLists={result} />
-      ) : (
-        <BlankHero />
-      )}
-      <Separator />
-      {categoryStatus ? (
-        <div className="md:grid grid-cols-3 gap-6">
-          {/* List Category */}
-          <div className="md:col-span-2 space-y-4">
-            {data && data.length > 0 ? (
-              data.map((category) => (
-                <React.Fragment key={category.name}>
-                  <CategoryHomePage
-                    category={category.data}
-                    categoryName={category.name}
-                  />
-                  <Separator />
-                </React.Fragment>
-              ))
-            ) : (
-              <div>Empty post</div>
-            )}
-          </div>
-          {/* sidebar */}
-          <div>
-            <PopularPosts />
-          </div>
-        </div>
-      ) : (
-        <div>Fail get data</div>
-      )}
-    </main>
-  );
+  // return (
+  //   <main className="space-y-4">
+  //     {headStatus && result.length > 0 ? (
+  //       <HeroSection headlinePostLists={result} />
+  //     ) : (
+  //       <BlankHero />
+  //     )}
+  //     <Separator />
+  //     {categoryStatus ? (
+  //       <div className="md:grid grid-cols-3 gap-6">
+  //         {/* List Category */}
+  //         <div className="md:col-span-2 space-y-4">
+  //           {data && data.length > 0 ? (
+  //             data.map((category) => (
+  //               <React.Fragment key={category.name}>
+  //                 <CategoryHomePage
+  //                   category={category.data}
+  //                   categoryName={category.name}
+  //                 />
+  //                 <Separator />
+  //               </React.Fragment>
+  //             ))
+  //           ) : (
+  //             <div>Empty post</div>
+  //           )}
+  //         </div>
+  //         {/* sidebar */}
+  //         <div>
+  //           <PopularPosts />
+  //         </div>
+  //       </div>
+  //     ) : (
+  //       <div>Fail get data</div>
+  //     )}
+  //   </main>
+  // );
 };
 
 export default WebHomePage;
